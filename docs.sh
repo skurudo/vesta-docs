24b8e9a698e3d41d396d2d707a2b6fa24a5ad924
Pavel Galkin <pavel@galkin.su>
2016-08-26 16:38:01 +0300

    second impact
    parser and result of parser work

---
+++ docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#delete file if exist
+rm docs.txt
+#find all files in VestaCP directory, get all description from command files and add in text file
+for file in $(find /usr/local/vesta/bin/$1 -type f | sort); do echo '';
+        echo $file >> /root/docs.txt;
+        sed -e '/./{H;$!d;}' -e 'x;/info/!d;/option/!d;/The\|This/!d' $file >> docs.txt;
+        echo -e "\n" >> docs.txt;
+done
+
+#delete /bin/bash lines
+sed -i '/bash/d' docs.txt;
